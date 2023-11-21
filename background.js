f8bfedf2e6e3b571378c0a0a33fbdc8a37091a19
colbyguan <colbyguan@gmail.com>
2023-11-21 16:31:04 -0500

    add confirm prompt with some from/to info

--- background.js
+++ background.js
@@ -20,34 +20,52 @@ chrome.action.onClicked.addListener(async (activeTab) => {
         })
     );
 
-    chrome.scripting.executeScript({
-      target: { tabId: activeTab.id },
-      function: (jwts) => console.log('FOUND JWTS', jwts),
-      args: [jwts],
-    });
-
-    await chrome.tabs.query({}).then(tabs => tabs.forEach(tab => {
+    const allTabs = await chrome.tabs.query({})
+    const localhostTabs = allTabs.filter(tab => {
       const host = PROTOCOLS.reduce((host, protocol) =>
         tab.url.startsWith(protocol) ? tab.url.slice(protocol.length) : host,
         undefined,
       );
-      if (host?.startsWith(TARGET_HOST)) {
-        jwts.forEach(jwt => chrome.cookies.set({ ...jwt, url: tab.url }));
-        chrome.scripting.executeScript({
-          target: { tabId: activeTab.id },
-          function: (jwts, url) => {
-            console.log('INJECTED', jwts, 'INTO', url);
-            alert(`INJECTED ${jwts.map(jwt => jwt.name + '\n' + jwt.value)} \nINTO ${url}`);
-          },
-          args: [jwts, tab.url],
-        });
-      }
-    }));
-  } catch(err) {
+      return tab.url !== activeTab.url && host?.startsWith(TARGET_HOST);
+    })
+
+    chrome.runtime.onMessage.addListener(
+      function handleConfirm(request) {
+        if (request.confirmed === true) {
+          localhostTabs.forEach(tab => {
+            jwts.forEach(jwt => chrome.cookies.set({ ...jwt, url: tab.url }));
+          })
+
+          chrome.scripting.executeScript({
+            target: { tabId: activeTab.id },
+            function: (jwts, urls) => {
+              const jwtsDisplay = jwts.map(jwt => `  ${jwt.name}=${jwt.value.slice(0, 30)}...`).join('\n')
+              alert(`INJECTED\n${jwtsDisplay}\nINTO\n${urls.map(url => '  ' + url).join('\n')}`);
+            },
+            args: [jwts, localhostTabs.map(tab => tab.url)],
+          });
+        }
+
+        if (typeof request.confirmed === 'boolean') {
+          chrome.runtime.onMessage.removeListener(handleConfirm)
+        }
+      });
+
     chrome.scripting.executeScript({
       target: { tabId: activeTab.id },
-      function: (err) => console.error('Error copying jwt', err),
+      function: (localhostTabs, activeTab) => {
+        const confirmed = confirm(`Inject JWT from this page (${activeTab.url}) into these tabs?\n\n${localhostTabs.map(tab => ' - ' + tab.url).join('\n')}`)
+        chrome.runtime.sendMessage({ confirmed }, null);
+      },
+      args: [localhostTabs, activeTab],
+    });
+
+
+  } catch (err) {
+    chrome.scripting.executeScript({
+      target: { tabId: activeTab.id },
+      function: (err) => alert('Error copying jwt'),
       args: [err],
-    }); 
+    });
   }
 });
